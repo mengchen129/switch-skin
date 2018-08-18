18387ac8054b8796604bfce0ce5fe17230bda682
mengchen <mengchen@zhimadj.com>
2018-08-18 10:56:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <button @click="switchStyle(1)">Yellow</button>
+        <button @click="switchStyle(2)">Orange</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        methods: {
+            switchStyle(index) {
+                let lastStyle = document.head.querySelector('style');
+                lastStyle && document.head.removeChild(lastStyle);
+                document.querySelector('#themeLink').href = `dist/style${index}.css`;
+                localStorage.setItem('DISPLAY_STYLE', index);
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+</style>
